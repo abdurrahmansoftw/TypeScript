6986bd178dcdb9d162cea03d689b39509e49b9c8
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-04 23:16:39 +0600

    update typescript

--- basic/src/script.ts
+++ basic/src/script.ts
@@ -1,21 +1,2 @@
-//  what is TypeScript:
-//  TypeScript is a superset of JavaScript that compiles to plain JavaScript.
-//  TypeScript is a typed superset of JavaScript that compiles to plain JavaScript.
-
-//  why use TypeScript:
-//  TypeScript can help prevent many potential runtime errors, especially as applications grow.
-//  TypeScript lets you use modern JavaScript features, like classes, modules, and interfaces.
-//  TypeScript supports tools for large-scale JavaScript applications for any browser, for any host, on any OS.
-//  TypeScript is open source.
-
-// whats wrong with JavaScript:
-//  JavaScript is a dynamic language.
-//  JavaScript is untyped.
-//  JavaScript is not object-oriented.
-//  JavaScript is not modular.
-
-// benefits of TypeScript:
-//  TypeScript is a typed superset of JavaScript that compiles to plain JavaScript.
-
-const country = 'I love bangladesh';
+const country = 'I love akaid';
 console.log(country);
