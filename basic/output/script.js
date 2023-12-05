6d8a8d9f09b15f0efb31fa6c7042d5ab81d19bb8
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-05 09:31:06 +0600

    class

--- basic/output/script.js
+++ basic/output/script.js
@@ -16,10 +16,31 @@
 //   return a + b;
 // };
 // console.log(myFunc(1, 2, true));
-const userDetails = (id, user) => {
-    console.log(`user id is ${id}, name is ${user.name} and age is ${user.age}`);
-};
-userDetails(1, { name: 'a', age: 4 });
-const sayHello = (user) => {
-    console.log(`hello ${user.age > 50 ? 'Sir' : 'Mr.'} ${user.name}`);
-};
+// const userDetails = (id: string | number, user: { name: string; age: number }) => {
+//   console.log(`user id is ${id}, name is ${user.name} and age is ${user.age}`);
+// };
+// userDetails(1, { name: 'a', age: 4 });
+// const sayHello = (user: { name: string; age: number }) => {
+//   console.log(`hello ${user.age > 50 ? 'Sir' : 'Mr.'} ${user.name}`);
+// };
+// function signature - type of function
+// let add: (x: number, y: number, z?: number) => number;
+// add = (a: number, b: number, c?: number) => {
+//   return a + b;
+// };
+// console.log(add(1, 2));
+class Player {
+    constructor(n, a, c) {
+        this.name = n;
+        this.age = a;
+        this.country = c;
+    }
+    play() {
+        console.log(`${this.name} from ${this.country} is playing, he is ${this.age} years old`);
+    }
+}
+const player1 = new Player('mosh', 40, 'Bangladesh');
+const player2 = new Player('alu', 40, 'Bangladesh');
+const player3 = new Player('potol', 40, 'Bangladesh');
+const players = [player1, player2, player3];
+console.log(players);
--- basic/src/script.ts
+++ basic/src/script.ts
@@ -21,12 +21,44 @@
 
 // console.log(myFunc(1, 2, true));
 
-const userDetails = (id: string | number, user: { name: string; age: number }) => {
-  console.log(`user id is ${id}, name is ${user.name} and age is ${user.age}`);
-};
+// const userDetails = (id: string | number, user: { name: string; age: number }) => {
+//   console.log(`user id is ${id}, name is ${user.name} and age is ${user.age}`);
+// };
 
-userDetails(1, { name: 'a', age: 4 });
+// userDetails(1, { name: 'a', age: 4 });
 
-const sayHello = (user: { name: string; age: number }) => {
-  console.log(`hello ${user.age > 50 ? 'Sir' : 'Mr.'} ${user.name}`);
-};
+// const sayHello = (user: { name: string; age: number }) => {
+//   console.log(`hello ${user.age > 50 ? 'Sir' : 'Mr.'} ${user.name}`);
+// };
+
+// function signature - type of function
+// let add: (x: number, y: number, z?: number) => number;
+
+// add = (a: number, b: number, c?: number) => {
+//   return a + b;
+// };
+
+// console.log(add(1, 2));
+
+class Player {
+  name: string;
+  age: number;
+  country: string;
+
+  constructor(n: string, a: number, c: string) {
+    this.name = n;
+    this.age = a;
+    this.country = c;
+  }
+
+  play() {
+    console.log(`${this.name} from ${this.country} is playing, he is ${this.age} years old`);
+  }
+}
+
+const player1 = new Player('mosh', 40, 'Bangladesh');
+const player2 = new Player('alu', 40, 'Bangladesh');
+const player3 = new Player('potol', 40, 'Bangladesh');
+
+const players: Player[] = [player1, player2, player3];
+console.log(players);
