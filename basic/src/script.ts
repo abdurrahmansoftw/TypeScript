e1c3d1f42574b4a1642e44270e48e0833cfaab2c
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-05 09:41:41 +0600

    typescript interface

--- basic/src/script.ts
+++ basic/src/script.ts
@@ -64,17 +64,39 @@
 // console.log(players);
 
 // access modifier
-class Player {
-  constructor(private name: string, public age: number, private country: string) {}
+// class Player {
+//   constructor(private name: string, public age: number, private country: string) {}
 
-  play() {
-    console.log(`${this.name} from ${this.country} is playing, he is ${this.age} years old`);
+//   play() {
+//     console.log(`${this.name} from ${this.country} is playing, he is ${this.age} years old`);
+//   }
+// }
+
+// const player1 = new Player('mosh', 40, 'use');
+// const player2 = new Player('alu', 40, 'india');
+// const player3 = new Player('potol', 40, 'Bangladesh');
+
+// const players: Player[] = [player1, player2, player3];
+// console.log(players.map((player) => player.play()));
+
+// typescript interface
+interface User {
+  name: string;
+  age: number;
+  country: string;
+  printDetails(): string;
+}
+
+class Player implements User {
+  constructor(public name: string, public age: number, public country: string) {}
+
+  printDetails() {
+    return `${this.name} from ${this.country} is playing, he is ${this.age} years old`;
   }
 }
 
-const player1 = new Player('mosh', 40, 'Bangladesh');
-const player2 = new Player('alu', 40, 'Bangladesh');
-const player3 = new Player('potol', 40, 'Bangladesh');
+const player1 = new Player('mosh', 40, 'use');
+const player2 = new Player('alu', 40, 'india');
 
-const players: Player[] = [player1, player2, player3];
-console.log(players.map((player) => player.play()));
+const players: User[] = [player1, player2];
+console.log(players.map((player) => player.printDetails()));
