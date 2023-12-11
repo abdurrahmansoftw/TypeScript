bccfe93c65609d23f4a46f8c30ed359234727d3b
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-11 17:58:19 +0600

    Any type

---
+++ advance/src/index.ts
@@ -0,0 +1,13 @@
+// let sales: number = 123_456_789;
+// let course: string = 'TypeScript';
+// let isFree: boolean = false;
+// let level;
+
+// function render(document: any) {
+//   console.log(document);
+// }
+
+// render(123);
+
+
+let number
