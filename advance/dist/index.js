aa43f2d7d5d845f1df620ccd9437d5423c5beb11
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-12 18:30:09 +0600

    Generics Interface

--- advance/dist/index.js
+++ advance/dist/index.js
@@ -1,12 +1,12 @@
 "use strict";
-class GoogleCalendar {
-    constructor(name) {
-        this.name = name;
-    }
-    addEvent() {
-        throw new Error('Method not implemented.');
-    }
-    removeEvent() {
-        throw new Error('Method not implemented.');
-    }
+var _a, _b;
+function fetch(url) {
+    return {
+        data: null,
+        error: null,
+    };
 }
+let user = fetch('url');
+(_a = user.data) === null || _a === void 0 ? void 0 : _a.userName;
+let product = fetch('url');
+(_b = product.data) === null || _b === void 0 ? void 0 : _b.title;
