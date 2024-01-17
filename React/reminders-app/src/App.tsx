a3f5da8b85ddbc1f2529627555f0c7df1a3c6a35
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-17 23:03:15 +0600

    NewReminder

--- React/reminders-app/src/App.tsx
+++ React/reminders-app/src/App.tsx
@@ -1,39 +1,39 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import NewReminder from './components/NewReminder';
-import ReminderList from './components/ReminderList';
-import Reminder from './models/reminders';
-import ReminderService from './services/remider'; // Import the ReminderService class
+import { useEffect, useState } from 'react'
+import './App.css'
+import NewReminder from './components/NewReminder'
+import ReminderList from './components/ReminderList'
+import Reminder from './models/reminders'
+import ReminderService from './services/remider' // Import the ReminderService class
 
 function App() {
-  const [reminders, setReminders] = useState<Reminder[]>([]);
+	const [reminders, setReminders] = useState<Reminder[]>([])
 
-  useEffect(() => {
-    loadReminders();
-  });
+	useEffect(() => {
+		loadReminders()
+	})
 
-  const loadReminders = async () => {
-    const reminders = await ReminderService.getReminders(); // Use the ReminderService class
-    setReminders(reminders);
-  };
+	const loadReminders = async () => {
+		const reminders = await ReminderService.getReminders() // Use the ReminderService class
+		setReminders(reminders)
+	}
 
-  const removeReminder = async (id: number) => {
-    await ReminderService.deleteReminder(id);
-    const newReminders = reminders.filter((reminder) => reminder.id !== id);
-    setReminders(newReminders);
-  };
+	const removeReminder = async (id: number) => {
+		await ReminderService.deleteReminder(id)
+		const newReminders = reminders.filter((reminder) => reminder.id !== id)
+		setReminders(newReminders)
+	}
 
-  const addReminder = async (title: string) => {
-    const newReminder = await ReminderService.addReminder(title);
-    setReminders([...reminders, newReminder]);
-  };
+	const addReminder = async (title: string) => {
+		const newReminder = await ReminderService.addReminder(title)
+		setReminders([...reminders, newReminder])
+	}
 
-  return (
-    <div className='App'>
-      <NewReminder onAddReminder={addReminder} />
-      <ReminderList items={reminders} onRempveReminder={removeReminder} />
-    </div>
-  );
+	return (
+		<div className='App'>
+			<NewReminder onAddReminder={addReminder} />
+			<ReminderList items={reminders} onRempveReminder={removeReminder} />
+		</div>
+	)
 }
 
-export default App;
+export default App
--- React/reminders-app/src/components/NewReminder.tsx
+++ React/reminders-app/src/components/NewReminder.tsx
@@ -1,27 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 
 interface NewReminderProps {
-  onAddReminder: (title: string) => void;
+	onAddReminder: (title: string) => void
 }
 
 function NewReminder({ onAddReminder }: NewReminderProps): JSX.Element {
-  const [title, setTitle] = useState('');
+	const [title, setTitle] = useState('')
 
-  const submitFrom = (e: React.FormEvent) => {
-    e.preventDefault();
-    onAddReminder(title);
-    setTitle('');
-  };
+	const submitFrom = (e: React.FormEvent) => {
+		e.preventDefault()
+		onAddReminder(title)
+		setTitle('')
+	}
 
-  return (
-    <form className='card p-5' onSubmit={submitFrom}>
-      <label htmlFor='title'>title</label>
-      <input value={title} onChange={(e) => setTitle(e.target.value)} id='title' type='text' className='form-control' />
-      <button type='submit' className='btn btn-primary rounded-pill my-3'>
-        Add New Reminder
-      </button>
-    </form>
-  );
+	return (
+		<form className='card p-5' onSubmit={submitFrom}>
+			<label htmlFor='title'>title</label>
+			<input
+				value={title}
+				onChange={(e) => setTitle(e.target.value)}
+				id='title'
+				type='text'
+				className='form-control'
+			/>
+			<button type='submit' className='btn btn-primary rounded-pill my-3'>
+				Add New Reminder
+			</button>
+		</form>
+	)
 }
 
-export default NewReminder;
+export default NewReminder
