998033f07b7c08d160309e3af89c8c5b28c92166
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-17 23:03:28 +0600

    ReminderList

--- React/reminders-app/src/components/ReminderList.tsx
+++ React/reminders-app/src/components/ReminderList.tsx
@@ -1,23 +1,26 @@
-import Reminder from '../models/reminders';
+import Reminder from '../models/reminders'
 
 interface ReminderListProps {
-  items: Reminder[];
-  onRempveReminder: (id: number) => void;
+	items: Reminder[]
+	onRempveReminder: (id: number) => void
 }
 
 function ReminderList({ items, onRempveReminder }: ReminderListProps) {
-  return (
-    <ol className='list-group'>
-      {items.map((item) => (
-        <li className='list-group-item' key={item.id}>
-          {item.title}
-          <button onClick={() => onRempveReminder(item.id)} className='btn btn-outline-danger mx-3 rounded-pill'>
-            Delete
-          </button>
-        </li>
-      ))}
-    </ol>
-  );
+	return (
+		<ol className='list-group'>
+			{items.map((item) => (
+				<li className='list-group-item' key={item.id}>
+					{item.title}
+					<button
+						onClick={() => onRempveReminder(item.id)}
+						className='btn btn-outline-danger mx-3 rounded-pill'
+					>
+						Delete
+					</button>
+				</li>
+			))}
+		</ol>
+	)
 }
 
-export default ReminderList;
+export default ReminderList
